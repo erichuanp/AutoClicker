2f8cd4574b33033eadaadacc8a0a44d848988bd6
Chuan Peng <ericpengchuan824@gmail.com>
2022-10-16 15:07:43 -0700

    update

--- Scripts/fixmine.py
+++ Scripts/fixmine.py
@@ -8,9 +8,9 @@ while True:
 
     for i in range(3):
         op.click(1)
-        op.click(0, 6)
+        op.click(0, 24)
         for j in range(20):
-            op.click(0, 3)
+            op.click(0, 12)
             op.delay(1.2)
     times += 1
     print('运行了' + str(times) + '次。')
